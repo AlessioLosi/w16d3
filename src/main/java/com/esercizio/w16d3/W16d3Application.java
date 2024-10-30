d6401ea486692bfafdf0b54fe1d23fc4522ad858
AlessioLosi <alessiolosi93@icloud.com>
2024-10-30 15:02:29 +0100

    inizio

---
+++ src/main/java/com/esercizio/w16d3/W16d3Application.java
@@ -0,0 +1,13 @@
+package com.esercizio.w16d3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W16d3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W16d3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/esercizio/w16d3/W16d3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esercizio.w16d3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W16d3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
